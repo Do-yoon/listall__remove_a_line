34f166b83e3dfe2d9adcf29b07d67e86d6a02908
cheeze <33196437+Do-yoon@users.noreply.github.com>
2019-03-27 10:00:42 +0900

    Create RemoveLine.py
    remove 1 line (which has .java file extension)

---
+++ RemoveLine.py
@@ -0,0 +1,30 @@
+#https://gist.github.com/jdan/1604413
+
+import os
+import sys
+import subprocess
+
+mylist = []
+
+def listdir(d):
+    if not os.path.isdir(d):
+        mylist.append(d)
+    else:
+        for item in os.listdir(d):
+            listdir((d + '/' + item) if d != '/' else '/' + item)
+
+if __name__ == '__main__':
+    if len(sys.argv) < 2:
+        print('Usage: listall DIR')
+    else:
+        listdir(sys.argv[1])
+
+#print(mylist)
+
+for myPath in mylist:
+    #f = open(myPath, 'w')
+    print(myPath)
+    #print(myPath[-4:] == 'java')
+    if myPath[-4:] == 'java':
+        subprocess.call(['sed', '-i', '1d', './' + myPath])
+    #f.close()
